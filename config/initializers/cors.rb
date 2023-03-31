869ddaa30fda63224e177453cd3d9dce3b214c57
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-03-31 18:14:20 +0300

    fix: fix problem with multiple cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,11 +8,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'http://localhost:8000'
     resource(
       '*',
       headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+      expose: ['access-token', 'expiry', 'token-type', 'Authorization', 'Access-Control-Allow-Origin'],
       methods: [:get, :patch, :put, :delete, :post, :options, :show]
     )
   end
