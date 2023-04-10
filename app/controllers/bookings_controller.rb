507308136feb00a680ab4b53f70464fa27c3df51
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-04-10 15:32:25 +0300

    feat: add update function in booking_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,16 +7,27 @@ class BookingsController < ApplicationController
 
 
   def create
-    a = current_user.id
+    current_user_id = current_user.id
     booking = Booking.create({
                  gym_id: params[:gym_id],
-                 user_id: a,
+                 user_id: current_user_id,
                  start_at: params[:start_at],
                  end_at: params[:end_at]
                })
     render json: booking
   end
 
+  def update
+    id = params[:id]
+    booking = Booking.find(id)
+    booking.update(
+      {
+        start_at: params[:start_at],
+        end_at: params[:end_at]
+      }
+    )
+    render json: booking
+  end
   def destroy
     id = params[:id]
     booking = Booking.find(id)
