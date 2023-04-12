4d5fbb56b6832711844865a9e16b3e36c0dd0a18
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-04-12 18:15:58 +0300

    feat: add check current_user on create booking in booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,14 +7,24 @@ class BookingsController < ApplicationController
 
 
   def create
-    current_user_id = current_user.id
-    booking = Booking.create({
-                 gym_id: params[:gym_id],
-                 user_id: current_user_id,
-                 start_at: params[:start_at],
-                 end_at: params[:end_at]
-               })
-    render json: booking
+    if current_user
+      current_user_id = current_user.id
+      booking = Booking.create({
+                                 gym_id: params[:gym_id],
+                                 user_id: current_user_id,
+                                 start_at: params[:start_at],
+                                 end_at: params[:end_at]
+                               })
+      render json: {
+        status: 200,
+        message: "booking was successfully on " + params[:start_at],
+      }, status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
   end
 
   def update
