37bd3fb050e62db99c6e29186770fc5777342e91
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-05-19 12:59:41 +0300

    feat: add hasAdmin function in current_controller

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -3,5 +3,18 @@ class CurrentUserController < ApplicationController
   def index
     render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
   end
+
+  def admin
+    current_user_role = current_user.role
+    if current_user_role == "admin"
+      render json: {
+        status: :ok,
+      }
+    else
+      render json: {
+        status: :unauthorized,
+      }
+    end
+  end
 end
 #serializeable_hash[:data][:attributes], status: :ok
