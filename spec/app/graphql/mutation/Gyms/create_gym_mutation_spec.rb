073dee12db174aa481baa08467dda61b7d634d71
trofimovmax <92295615+TrofimovMax@users.noreply.github.com>
2023-08-14 18:11:43 +0300

    feat: add three spec test for CreateGymMutation

--- spec/app/graphql/mutation/Gyms/create_gym_mutation_spec.rb
+++ spec/app/graphql/mutation/Gyms/create_gym_mutation_spec.rb
@@ -6,23 +6,31 @@ module Mutations
   module Gyms
     RSpec.describe Mutations::CreateGymMutation, type: :request do
       let(:gym) do
-        Gym.create(id: 1, title: 'AlexFitness', address: 'Rostov on Don, Krasnoarmeyskaya 80', capacity: 10, owner_id: nil)
+        Gym.create(id: 1,
+                   title: 'AlexFitness',
+                   address: 'Rostov on Don, Krasnoarmeyskaya 80',
+                   capacity: 10,
+                   owner_id: nil)
       end
 
       describe '.resolve' do
         it 'creates a gym' do
-          :gym
-
+          gym_title = gym.title
+          gym_address = gym.address
+          gym_capacity = gym.capacity
           expect do
-            post '/graphql', params: { query: query }
+            post '/graphql', params: { query: query(title: gym_title,
+                                                    address: gym_address,
+                                                    capacity: gym_capacity) }
           end.to change { Gym.count }.by(1)
         end
 
         it 'returns a gym' do
-          post '/graphql', params: { query: query }
+          post '/graphql', params: { query: query(title: gym.title,
+                                                  address: gym.address,
+                                                  capacity: gym.capacity) }
           json = JSON.parse(response.body)
           data = json['data']['createGymMutation']['gym']
-
           expect(data).to include(
                             'id' => be_present,
                             'title' => 'AlexFitness',
@@ -30,19 +38,42 @@ module Mutations
                             'capacity' => 10
                           )
         end
-
       end
 
-      def query
+      describe "returns an error when required parameters are missing" do
+        it 'returns an error when required parameters are missing all parameters' do
+          post '/graphql', params: { query: query(title: nil, address: nil) }
+          json = JSON.parse(response.body)
+          error_message = json['errors'][0]["message"]
+
+          expect(error_message).to eq("Parse error on \"}\" (RCURLY) at [7, 5]")
+        end
+        it 'returns an error when required parameters are missing title parameter' do
+          post '/graphql', params: { query: query(title: nil, address: "Rostov on Don, Krasnoarmeyskaya 80") }
+          json = JSON.parse(response.body)
+          error_message = json['errors'][0]["message"]
+
+          expect(error_message).to eq("Parse error on \"}\" (RCURLY) at [7, 5]")
+        end
+        it 'returns an error when required parameters are missing address parameters' do
+          post '/graphql', params: { query: query(title: "AlexFitness", address: nil) }
+          json = JSON.parse(response.body)
+          error_message = json['errors'][0]["message"]
+
+          expect(error_message).to eq("Parse error on \"}\" (RCURLY) at [7, 5]")
+        end
+      end
+
+      def query(title:, address:, capacity: nil)
         <<~GQL
-          mutation create {
-              createGymMutation(input: {
-                attributes: {
-                  title: "AlexFitness"
-                	address: "Rostov on Don, Krasnoarmeyskaya 80"
-                  capacity: 10
-                }
-              }){
+          mutation {
+            createGymMutation(input: {
+              attributes: {
+                title: "#{title}",
+                address: "#{address}",
+                capacity: #{capacity}
+              }
+            }) {
               gym {
                 id
                 title
